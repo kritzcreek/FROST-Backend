78e47c754da80f3cd8943301a19817286330cde4
kRITZCREEK <christoph.hegemann@gmx.net>
2015-03-12 10:44:32 +0100

    cleanup.

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -8,17 +8,9 @@ import           Yesod
 import           Yesod.Static
 
 data SocketState = SocketState { appState :: TVar AppState,  broadcastChan :: TChan ByteString }
-
 data App = App { conpool :: ConnectionPool, socketState :: SocketState, getStatic :: Static }
 
 staticFiles "static"
-
-{-
-
-The Yesod typeclass allows us to alter a number of the behaviors of Yesod, such
-as the default page layout, error handler functions, and how URLs are rendered.
-
--}
 
 instance Yesod App
 
@@ -28,7 +20,6 @@ instance YesodPersist App where
     runDB action = do
         App pool _ _ <- getYesod
         runSqlPool action pool
-
 
 mkYesodData "App" [parseRoutesNoCheck|
 /rooms                      RoomsR     GET POST
