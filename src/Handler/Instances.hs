0a52f46d7f02f02fb0515b7281b1d8d8331594ff
creek <christoph.hegemann@gmx.net>
2015-04-19 00:24:26 +0200

    reformatting and cleanup

--- src/Handler/Instances.hs
+++ src/Handler/Instances.hs
@@ -1,18 +1,19 @@
-module Handler.Instances 
+module Handler.Instances
 ( getInstancesR
 , postInstancesR
 , getInstanceR
 , getInstanceMobileR
 , keys
-, unpack) where
+, unwrap) where
 
-import Import
-import Application.Types
-import qualified Data.Map as M
-import Data.Text (Text())
-import Control.Concurrent.STM
-import Network.HTTP.Types
-import System.FilePath.Posix((</>))
+import           Application.Types
+import           Control.Applicative
+import           Control.Concurrent.STM
+import qualified Data.Map               as M
+import           Data.Text              (Text ())
+import           Import
+import           Network.HTTP.Types
+import           System.FilePath.Posix  ((</>))
 
 mkNewId :: App -> STM InstanceId
 mkNewId application = do
@@ -21,19 +22,16 @@ mkNewId application = do
 
 initNewInstance :: App -> InstanceId -> STM ()
 initNewInstance application iid = do
-  oldState <- readTVar $ socketStates'
-  newState   <- newTVar myState
-  channel <- newBroadcastTChan
-  writeTVar socketStates' (M.insert iid (SocketState newState channel) oldState)
+  oldStates <- readTVar socketStates'
+  newState <- SocketState <$> newTVar myState <*> newBroadcastTChan
+  writeTVar socketStates' (M.insert iid newState oldStates)
   where socketStates' = socketStates application
 
-unpack :: InstanceId -> Integer
-unpack (InstanceId iid) = iid
+unwrap :: InstanceId -> Integer
+unwrap (InstanceId iid) = iid
 
 keys :: App -> STM [Integer]
-keys app = do
-  readTVar (socketStates app) >>= return . map unpack . M.keys
-
+keys app = return . map unwrap . M.keys =<< readTVar (socketStates app)
 
 getInstancesR :: Handler Value
 getInstancesR = do
@@ -55,12 +53,13 @@ getInstance :: FilePath -> InstanceId -> Handler Html
 getInstance path instanceId = do
   application <- getYesod
   ks <- liftIO $ atomically $ keys application
-  if unpack instanceId `elem` ks
+  if unwrap instanceId `elem` ks
     then sendFile "text/html" $ "static" </> path </> "index.html"
     else sendResponseStatus status404 ("Instance doesn't exist" :: Text)
 
 getInstanceR :: InstanceId -> Handler Html
-getInstanceR = getInstance "" 
+getInstanceR = getInstance ""
 
 getInstanceMobileR :: InstanceId -> Handler Html
 getInstanceMobileR = getInstance "mobile"
+
