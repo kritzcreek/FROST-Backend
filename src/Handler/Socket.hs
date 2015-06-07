73f67f33a3e60862ed95d1f3fc166ad20bf0712e
kRITZCREEK <christoph.hegemann@gmx.net>
2015-06-08 00:14:40 +0200

    refactor Websocket Handlers
    so that they the corresponding InstanceID gets embedded in a Reader Monad

--- src/Handler/Socket.hs
+++ src/Handler/Socket.hs
@@ -8,6 +8,7 @@ import           Application.Types
 import           Control.Applicative
 import           Control.Concurrent.STM
 import           Control.Monad          (forever)
+import           Control.Monad.Trans.Reader
 import           Network.HTTP.Types
 import           Data.Aeson
 import           Data.ByteString.Lazy   (ByteString)
@@ -17,13 +18,15 @@ import           Debug.Trace
 import           Import
 import           Yesod.WebSockets
 
+type ClientHandler = ReaderT InstanceId (WebSocketsT Handler)
+
 getServerState :: InstanceId -> Handler (TVar AppState)
 getServerState = getFromSocketstate appState
 
 getBroadcastChannel :: InstanceId -> Handler (TChan ByteString)
 getBroadcastChannel = getFromSocketstate broadcastChan
 
-getFromSocketstate ::(SocketState -> a) ->  InstanceId -> Handler a
+getFromSocketstate ::(SocketState -> a) -> InstanceId -> Handler a
 getFromSocketstate accessor instanceId = do
   App _ states _ <- getYesod
   sts <- liftIO $ atomically $ M.lookup instanceId <$> readTVar states
@@ -33,48 +36,46 @@ getFromSocketstate accessor instanceId = do
 debugger :: ByteString -> WebSocketsT Handler ()
 debugger a = return $ trace (show a) ()
 
-handleCommand :: InstanceId -> Command -> WebSocketsT Handler ByteString
-handleCommand instanceId RequestState = do
-  serverState <- lift $ getServerState instanceId
+handleCommand :: Command -> ClientHandler ByteString
+handleCommand RequestState = do
+  serverState <- lift . lift . getServerState =<< ask
   liftIO $ atomically $ do
     events <- generateEvents <$> readTVar serverState
     return $ encode (ReplayEvents events)
-handleCommand _ (Echo s) = return (encode s)
+handleCommand (Echo s) = return (encode s)
 
 logSomething :: Show a => a -> WebSocketsT Handler ()
 logSomething a = $(logInfo) $ T.pack (show a)
 
-applyEvent :: InstanceId -> Event -> WebSocketsT Handler ()
-applyEvent instanceId e = do
-  serverState <- lift $ getServerState instanceId
+applyEvent :: Event -> ClientHandler ()
+applyEvent e = do
+  serverState <- lift . lift . getServerState =<< ask
   liftIO $ atomically $ do
     newState <- evalEvent e <$> readTVar serverState
     writeTVar serverState newState
 
-handleEvent :: InstanceId -> Event -> WebSocketsT Handler ByteString
-handleEvent instanceId e = do
-  logSomething e
-  applyEvent instanceId e
+handleEvent :: Event -> ClientHandler ByteString
+handleEvent e = do
+  lift $ logSomething e
+  applyEvent e
   return (encode e)
 
-openSpaceApp :: InstanceId -> WebSocketsT Handler ()
-openSpaceApp instanceId = do
-  writeChan <- lift $ getBroadcastChannel instanceId
+openSpaceApp :: ClientHandler ()
+openSpaceApp = do
+  writeChan <- lift .lift . getBroadcastChannel =<< ask
   readChan  <- liftIO $ atomically $ dupTChan writeChan
   race_
         (forever $ do
           msg <- liftIO $ atomically (readTChan readChan)
-          sendTextData msg)
+          lift $ sendTextData msg)
         (forever $ do
-          action <- receiveData
-          logSomething action
+          action <- lift receiveData
+          lift $ logSomething action
           case decode action of
-            Just (e :: Event) -> handleEvent' e >>= liftIO . atomically . writeTChan writeChan
+            Just (e :: Event) -> handleEvent e >>= liftIO . atomically . writeTChan writeChan
             Nothing -> case decode action of
-              Just (c :: Command) -> handleCommand' c >>= sendTextData
+              Just (c :: Command) -> handleCommand c >>= lift . sendTextData
               Nothing -> return ())
-  where handleCommand' = handleCommand instanceId
-        handleEvent' = handleEvent instanceId
 
 handleSocketR :: InstanceId -> Handler ()
-handleSocketR instanceId = webSockets $ openSpaceApp instanceId
+handleSocketR instanceId = webSockets $ runReaderT openSpaceApp instanceId
